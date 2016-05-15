fcba2b86da336142782be30657640f295efd10b8
avtor31 <avtor31@yandex.ru>
2016-05-15 21:25:09 +0300

    Greating has prepared in application.

--- src/com/epam/epetrov/calculator/Main.java
+++ src/com/epam/epetrov/calculator/Main.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        Main app = new Main();
+        System.out.println("\t Hi!\n It is a summing calculator.");
+        String endApp = "";
     }
-
 }
