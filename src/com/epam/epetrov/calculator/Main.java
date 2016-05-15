af8771d28117426caffbad2bd9506b124c46b2bc
avtor31 <avtor31@yandex.ru>
2016-05-15 21:29:54 +0300

    Summing of two operands has been created

--- src/com/epam/epetrov/calculator/Main.java
+++ src/com/epam/epetrov/calculator/Main.java
@@ -8,5 +8,25 @@ public class Main {
         Main app = new Main();
         System.out.println("\t Hi!\n It is a summing calculator.");
         String endApp = "";
+        while(!endApp.equalsIgnoreCase("c")) {
+            endApp = app.run();
+        }
+    }
+    private String run() {
+        Scanner keyboard = new Scanner(System.in);
+        int firstValue = 0;
+        int secondValue = 0;
+        try{
+            System.out.println("enter an integer:");
+            firstValue = keyboard.nextInt();
+            System.out.println("enter a next integer:");
+            secondValue = keyboard.nextInt();
+        } catch (Exception e) {
+            System.out.println("the value is not an integer!");
+            this.run();
+        }
+        System.out.println(String.format("The summ of %d and %d is: %d", firstValue,  secondValue, firstValue + secondValue));
+        System.out.println("\t If you want to get off an application enter symbol \"C\"\n in other way enter any symbol: ");
+        return keyboard.next();
     }
 }
